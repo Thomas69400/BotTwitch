7d6378c81cca40ab24e36a19f761398479a63f5d
Naitchi <assasindu39@gmail.com>
2024-07-29 00:40:27 +0200

    ♻️ Refactor des fonctions QuiQuoiPourquoi

--- functions.js
+++ functions.js
@@ -1,4 +1,87 @@
-export const convertWords = (words) => {
-    words = words.filter(word => {if (word.length > 0) return word; });
-    return words.reverse();
-}
+import { randomInt } from 'crypto';
+
+const userCooldowns = {}; // Object to store the last response times for users
+const COOLDOWN_TIME = 10 * 60 * 1000; // 5 minutes in milliseconds
+
+export const deleteEmptyWords = (words) => {
+  words = words.filter((word) => {
+    if (word.length > 0) return word;
+  });
+  return words.reverse();
+};
+
+// CoolDown pour pas que le bot spam les quette et feur à la même personne
+export const checkCooldown = (user) => {
+  const now = Date.now();
+  if (!userCooldowns[user]) {
+    userCooldowns[user] = now;
+    return false;
+  }
+  const lastResponseTime = userCooldowns[user];
+  if (now - lastResponseTime > COOLDOWN_TIME) {
+    userCooldowns[user] = now;
+    return false;
+  }
+  return true;
+};
+
+// Répondre FEUR après un 'pourquoi' en fin de phrase
+export const checkForPourquoi = (client, channel, message, tags) => {
+  const pourquoiRegex = /\bpourqu?o+i+\b/g;
+  const replacedMessage = message.replace(pourquoiRegex, 'pourquoi');
+  const responsesPourquoi = [
+    'Tout simplement pour feur Pepega Pepega',
+    'Pour feur :joy: :rofl: :rofl:',
+    "Peut-être bien que c'est pour coubeh !",
+    "Il me semble que c'est bien pour feur. Buratino",
+  ];
+
+  if (replacedMessage.search('pourquoi') !== -1) {
+    let words = replacedMessage.split(' ');
+    words = deleteEmptyWords(words);
+    if (words[0] === 'pourquoi') {
+      if (!checkCooldown(tags.username)) {
+        client.reply(channel, responsesPourquoi[randomInt(responsesPourquoi.length)], tags.id);
+      }
+    }
+  }
+};
+
+// Répondre FEUR après un 'quoi' en fin de phrase
+export const checkForQuoi = (client, channel, message, tags) => {
+  const quoiRegex = /\bqu?o+i+\b/g;
+  const replacedMessage = message.replace(quoiRegex, 'pourquoi');
+  const responsesQuoi = ['FEUR !!!!!', "Feur (coiffeur tu l'as ou pas ^^ ?)"];
+
+  if (replacedMessage.search('quoi') !== -1) {
+    let words = replacedMessage.split(' ');
+    words = deleteEmptyWords(words);
+    if (words[0] === 'quoi') {
+      if (!checkCooldown(tags.username)) {
+        client.reply(channel, responsesQuoi[randomInt(responsesQuoi.length)], tags.id);
+      }
+    }
+  }
+};
+
+// Répondre Quette après un 'qui' en fin de phrase
+export const checkForQui = (client, channel, message, tags) => {
+  const quiRegex = /\bqu?i+\b/g;
+  const replacedMessage = message.replace(quiRegex, 'qui');
+
+  const responsesQui = [
+    'Quette ou bien kette tel est la question :thinking:',
+    "C'est quette MonkeySpin",
+    "Quette (quiquette tu l'as ou pas ^^ ?)",
+  ];
+
+  if (replacedMessage.search('qui') !== -1) {
+    let words = replacedMessage.split(' ');
+    words = deleteEmptyWords(words);
+    if (words[0] === 'qui') {
+      if (!checkCooldown(tags.username)) {
+        client.reply(channel, responsesQui[randomInt(responsesQui.length)], tags.id);
+      }
+    }
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import http from 'http';
-import dotenv from 'dotenv';
-import route from './route.js';
 import tmi from 'tmi.js';
-import { randomInt } from 'crypto';
-import { convertWords } from './functions.js';
+import route from './route.js';
+
+import { checkCooldown, checkForPourquoi, checkForQui, checkForQuoi } from './functions.js';
 
 dotenv.config();
 
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Utilisation du routeur importé
 app.use('/', route);
 
 server.listen(PORT, () => {
@@ -31,70 +30,19 @@ const client = new tmi.Client({
 });
 client.connect().catch(console.error);
 
-const userCooldowns = {}; // Object to store the last response times for users
-const COOLDOWN_TIME = 10 * 60 * 1000; // 5 minutes in milliseconds
-const quoiRegex = /\bqu?o+i+\b/g; // REGEX regarde si "quoi" avec plusieurs "i"
-const pourquoiRegex = /\bpourqu?o+i+\b/g; // REGEX regarde si "pourquoi" avec plusieurs "i"
-const quiRegex = /\bqu?i+\b/g; // REGEX regarde si "pourquoi" avec plusieurs "i"
-const onlyLetter = /[^a-z\s]/g; // REGEX prend que les lettres et les espaces
-const responsesPourquoi = [
-  `Tout simplement pour feur Pepega Pepega`,
-  `Pour feur :joy: :rofl: :rofl:`,
-  `Peut-être bien que c'est pour coubeh !`,
-  `Il me semble que c'est bien pour feur. Buratino`,
-];
-const responsesQuoi = [`FEUR !!!!!`, `Feur (coiffeur tu l'as ou pas ^^ ?)`];
-const responsesQui = [
-  'Quette ou bien kette tel est la question :thinking:',
-  "C'est quette MonkeySpin",
-  `Quette (quiquette tu l'as ou pas ^^ ?)`,
-];
+// Regex
+const onlyLetter = /[^a-z\s]/g;
 
 client.on('message', (channel, tags, message, self) => {
+  // Ne répond pas à soit-même
   if (self) return;
+
+  // Supprime tout les caractères spéciaux
   const trunkMessage = message.toLowerCase().replace(onlyLetter, '');
-  // CD pour pas que le bot spam les quette et feur
-  const checkCooldown = (user) => {
-    const now = Date.now();
-    if (!userCooldowns[user]) {
-      userCooldowns[user] = now;
-      return false; // No cooldown in effect
-    }
-    const lastResponseTime = userCooldowns[user];
-    if (now - lastResponseTime > COOLDOWN_TIME) {
-      userCooldowns[user] = now;
-      return false; // Cooldown period has passed
-    }
-    return true; // Cooldown in effect
-  };
-  // Répondre FEUR en fin de phrase après un 'pourquoi'
-  if (trunkMessage.replace(pourquoiRegex, 'pourquoi').search('pourquoi') !== -1) {
-    let words = trunkMessage.replace(pourquoiRegex, 'pourquoi').split(' ');
-    words = convertWords(words);
-    if (words[0] === 'pourquoi') {
-      if (!checkCooldown(tags.username)) {
-        client.reply(channel, responsesPourquoi[randomInt(responsesPourquoi.length)], tags.id);
-      }
-    }
-  }
-  // Répondre FEUR après un 'quoi' en fin de phrase
-  if (trunkMessage.replace(quoiRegex, 'quoi').search('quoi') !== -1) {
-    let words = trunkMessage.replace(quoiRegex, 'quoi').split(' ');
-    words = convertWords(words);
-    if (words[0] === 'quoi') {
-      if (!checkCooldown(tags.username)) {
-        client.reply(channel, responsesQuoi[randomInt(responsesQuoi.length)], tags.id);
-      }
-    }
-  }
-  // Répondre Quette en fin de phrase après un 'qui'
-  if (trunkMessage.replace(quiRegex, 'quoi').search('quoi') !== -1) {
-    let words = trunkMessage.replace(quiRegex, 'qui').split(' ');
-    words = convertWords(words);
-    if (words[0] === 'qui') {
-      if (!checkCooldown(tags.username)) {
-        client.reply(channel, responsesQui[randomInt(responsesQui.length)], tags.id);
-      }
-    }
-  }
+
+  checkCooldown();
+
+  checkForPourquoi(client, channel, trunkMessage, tags);
+  checkForQuoi(client, channel, trunkMessage, tags);
+  checkForQui(client, channel, trunkMessage, tags);
 });
