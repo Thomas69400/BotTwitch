726b6eee41e54383159e6c4581fce63fb7d3ea58
Thomas69400 <bbstryllogy@gmail.com>
2024-07-28 20:33:51 +0200

    Fonction feur avec quoi et pourquoi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import http from 'http';
 import dotenv from 'dotenv';
 import route from './route.js'; // Assurez-vous que le chemin est correct
 import tmi from 'tmi.js';
+import { log } from 'console';
 
 dotenv.config();
 
@@ -19,18 +20,33 @@ server.listen(PORT, () => {
     console.log(`Serveur lancé sur le PORT ${PORT}`);
 });
 
+const onlyLetter = /[^a-zA-Z\s]/g; //REGEX
+
 const client = new tmi.Client({
 	options: { debug: true },
 	identity: {
-		username: 'TryllogexBot',
+		username: 'LytchiBot',
 		password: process.env.TOKEN,
 	},
-	channels: [ 'Tryllogy' ]
+	channels: [ 'Tryllogy', 'Naitchi' ]
 });
 client.connect().catch(console.error);
 client.on('message', (channel, tags, message, self) => {
+
 	if(self) return;
 	if(message.toLowerCase() === '!hello') {
 		client.say(channel, `@${tags.username}, heya!`);
 	}
+	if(message.toLowerCase().replace(onlyLetter, "").search("quoi") !== -1) {
+	 	var words = message.toLowerCase().replace(onlyLetter, "").split(' ');
+		words = words.filter(word => {if (word.length > 0) return word; });
+		words.reverse();
+		if(words[0] === "quoi") { client.say(channel, `FEUR !`) };
+	}
+	if(message.toLowerCase().replace(onlyLetter, "").search("pourquoi") !== -1) {
+		var words = message.toLowerCase().replace(onlyLetter, "").split(' ');
+	   words = words.filter(word => {if (word.length > 0) return word; });
+	   words.reverse();
+	   if(words[0] === "pourquoi") { client.say(channel, `Il me semble que c'est bien pour feur @${tags.username}. Buratino`) };
+   }
 });
