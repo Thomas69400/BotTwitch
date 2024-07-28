d5aa789070e138e752c53d7a12a529a7cf39c711
Thomas69400 <bbstryllogy@gmail.com>
2024-07-29 00:06:52 +0200

    💄 Ajout de Nodemon + Formatage index

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ app.use(express.json());
 app.use('/', route);
 
 server.listen(PORT, () => {
-    console.log(`Serveur lancé sur le PORT ${PORT}`);
+  console.log(`Serveur lancé sur le PORT ${PORT}`);
 });
 
 const client = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: 'LytchiBot',
-		password: process.env.TOKEN,
-	},
-	channels: [ process.env.CHANNEL ]
+  options: { debug: true },
+  identity: {
+    username: 'LytchiBot',
+    password: process.env.TOKEN,
+  },
+  channels: [process.env.CHANNEL],
 });
 client.connect().catch(console.error);
 
@@ -38,66 +38,63 @@ const pourquoiRegex = /\bpourqu?o+i+\b/g; // REGEX regarde si "pourquoi" avec pl
 const quiRegex = /\bqu?i+\b/g; // REGEX regarde si "pourquoi" avec plusieurs "i"
 const onlyLetter = /[^a-z\s]/g; // REGEX prend que les lettres et les espaces
 const responsesPourquoi = [
-	`Tout simplement pour feur Pepega Pepega`,
-	`Pour feur :joy: :rofl: :rofl:`,
-	`Peut-être bien que c'est pour coubeh !`,
-	`Il me semble que c'est bien pour feur. Buratino`
+  `Tout simplement pour feur Pepega Pepega`,
+  `Pour feur :joy: :rofl: :rofl:`,
+  `Peut-être bien que c'est pour coubeh !`,
+  `Il me semble que c'est bien pour feur. Buratino`,
 ];
-const responsesQuoi = [
-	`FEUR !!!!!`,
-	`Feur (coiffeur tu l'as ou pas ^^ ?)`
-];
+const responsesQuoi = [`FEUR !!!!!`, `Feur (coiffeur tu l'as ou pas ^^ ?)`];
 const responsesQui = [
-	'Quette ou bien kette tel est la question :thinking:',
-	"C'est quette MonkeySpin"
+  'Quette ou bien kette tel est la question :thinking:',
+  "C'est quette MonkeySpin",
+  `Quette (quiquette tu l'as ou pas ^^ ?)`,
 ];
 
 client.on('message', (channel, tags, message, self) => {
-	if(self) return;
-	console.log(tags);
-	const trunkMessage = message.toLowerCase().replace(onlyLetter, "");
-	// CD pour pas que le bot spam les quette et feur
-	const checkCooldown = (user) => {
-		const now = Date.now();
-		if (!userCooldowns[user]) {
-			userCooldowns[user] = now;
-			return false; // No cooldown in effect
-		}
-		const lastResponseTime = userCooldowns[user];
-		if (now - lastResponseTime > COOLDOWN_TIME) {
-			userCooldowns[user] = now;
-			return false; // Cooldown period has passed
-		}
-		return true; // Cooldown in effect
-	};
-	// Répondre FEUR en fin de phrase après un 'pourquoi'
-	if(trunkMessage.replace(pourquoiRegex, 'pourquoi').search("pourquoi") !== -1) {
-	 	let words = trunkMessage.replace(pourquoiRegex, 'pourquoi').split(' ');
-		words = convertWords(words);
-		if(words[0] === 'pourquoi') {
-			if (!checkCooldown(tags.username)) {
-				client.reply(channel, responsesPourquoi[randomInt(responsesPourquoi.length)], tags.id);
-			}
-		};
-	}
-	// Répondre FEUR après un 'quoi' en fin de phrase
-	if(trunkMessage.replace(quoiRegex, 'quoi').search("quoi") !== -1) {
-	 	let words = trunkMessage.replace(quoiRegex, 'quoi').split(' ');
-		words = convertWords(words);
-		if(words[0] === "quoi") { 
-			if (!checkCooldown(tags.username)) {
-				client.reply(channel, responsesQuoi[randomInt(responsesQuoi.length)], tags.id);
-			}
-		};
-	}
-	// Répondre Quette en fin de phrase après un 'qui'
-	if(trunkMessage.replace(quiRegex, 'quoi').search("quoi") !== -1) {
-	 	let words = trunkMessage.replace(quiRegex, 'qui').split(' ');
-		words = convertWords(words);
-		if(words[0] === "qui") { 
-			if (!checkCooldown(tags.username)) {
-				client.reply(channel, responsesQui[randomInt(responsesQui.length)], tags.id);
-			}
-		};
-	}
+  if (self) return;
+  const trunkMessage = message.toLowerCase().replace(onlyLetter, '');
+  // CD pour pas que le bot spam les quette et feur
+  const checkCooldown = (user) => {
+    const now = Date.now();
+    if (!userCooldowns[user]) {
+      userCooldowns[user] = now;
+      return false; // No cooldown in effect
+    }
+    const lastResponseTime = userCooldowns[user];
+    if (now - lastResponseTime > COOLDOWN_TIME) {
+      userCooldowns[user] = now;
+      return false; // Cooldown period has passed
+    }
+    return true; // Cooldown in effect
+  };
+  // Répondre FEUR en fin de phrase après un 'pourquoi'
+  if (trunkMessage.replace(pourquoiRegex, 'pourquoi').search('pourquoi') !== -1) {
+    let words = trunkMessage.replace(pourquoiRegex, 'pourquoi').split(' ');
+    words = convertWords(words);
+    if (words[0] === 'pourquoi') {
+      if (!checkCooldown(tags.username)) {
+        client.reply(channel, responsesPourquoi[randomInt(responsesPourquoi.length)], tags.id);
+      }
+    }
+  }
+  // Répondre FEUR après un 'quoi' en fin de phrase
+  if (trunkMessage.replace(quoiRegex, 'quoi').search('quoi') !== -1) {
+    let words = trunkMessage.replace(quoiRegex, 'quoi').split(' ');
+    words = convertWords(words);
+    if (words[0] === 'quoi') {
+      if (!checkCooldown(tags.username)) {
+        client.reply(channel, responsesQuoi[randomInt(responsesQuoi.length)], tags.id);
+      }
+    }
+  }
+  // Répondre Quette en fin de phrase après un 'qui'
+  if (trunkMessage.replace(quiRegex, 'quoi').search('quoi') !== -1) {
+    let words = trunkMessage.replace(quiRegex, 'qui').split(' ');
+    words = convertWords(words);
+    if (words[0] === 'qui') {
+      if (!checkCooldown(tags.username)) {
+        client.reply(channel, responsesQui[randomInt(responsesQui.length)], tags.id);
+      }
+    }
+  }
 });
