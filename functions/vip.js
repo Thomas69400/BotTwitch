3e510c968896ba39ef0f6dcca454cdc76768d540
Thomas69400 <bbstryllogy@gmail.com>
2024-08-01 19:36:52 +0200

    🚧 Ajouts des vips

--- functions/vip.js
+++ functions/vip.js
@@ -1,19 +1,5 @@
-import axios from 'axios';
-import { getOauthTokenBroadcaster } from '../auth.js';
+import { serviceMakeVip, serviceRemoveVip } from '../services/vip.js';
 
-export const makeVip = async () => {
-  const oauthToken = await getOauthTokenBroadcaster();
-  const url = `https://api.twitch.tv/helix/channels/vips?broadcaster_id=${process.env.BROADCASTER_ID}&user_id=89904723`;
-  const headers = {
-    Authorization: `Bearer ${oauthToken}`,
-    'Client-Id': process.env.CLIENTID,
-  };
-  try {
-    const response = await axios.post(url, {}, { headers });
-    console.log('Response:', response.data);
-  } catch (error) {
-    console.error('Error in vip function:', error.response ? error.response.data : error.message);
-  }
-};
+export const makeVip = async () => {};
 
 export const removeVip = async () => {};
---
+++ services/vip.js
@@ -0,0 +1,19 @@
+import { getOauthTokenBroadcaster } from './auth.js';
+import axios from 'axios';
+
+export const serviceMakeVip = async () => {
+  const oauthToken = await getOauthTokenBroadcaster();
+  const url = `https://api.twitch.tv/helix/channels/vips?broadcaster_id=${process.env.BROADCASTER_ID}&user_id=89904723`;
+  const headers = {
+    Authorization: `Bearer ${oauthToken}`,
+    'Client-Id': process.env.CLIENTID,
+  };
+  try {
+    const response = await axios.post(url, {}, { headers });
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('Error in vip function:', error.response ? error.response.data : error.message);
+  }
+};
+
+export const serviceRemoveVip = async () => {};
