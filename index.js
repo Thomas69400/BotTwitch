8d6b0e796ef9714c0f02afada48f42bb01e5773a
Thomas69400 <bbstryllogy@gmail.com>
2024-08-02 15:15:58 +0200

    ✨ Changement pour le self. Le bot peut lui meme lancer des raffles

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ import {
   checkForQuoi,
 } from './functions/whoWhyWhat.js';
 import { activeRevenue, checkViewers, readFile } from './functions/points.js';
-import { startRaffle, cancelRaffle, joinRaffle } from './functions/raffle.js';
+import { startRaffle, cancelRaffle, joinRaffle, begForRaffle } from './functions/raffle.js';
 import { timeout } from './functions/timeout.js';
 import { getOauthToken } from './services/auth.js';
+import { allCommandes } from './functions/utils.js';
 
 // Initialisation
 const client = new tmi.Client({
@@ -28,24 +29,25 @@ readFile();
 // Regex
 const onlyLetter = /[^a-z\s]/g;
 const letterNumber = /[^a-z1-9\s]/;
+const containsBeg = /\bbeg\b/i;
+const containsRaffle = /\braffle\b/i;
 
 client.on('message', async (channel, tags, message, self) => {
-  // Le bot ne répond pas à lui-même
-  if (self) return;
-
   // Si le spectateur n'est pas déjà suivi par le systeme de point, l'ajoute
   checkViewers(tags);
   // Supprime tout les caractères spéciaux
   const trunkMessage = message.toLowerCase().replace(onlyLetter, '');
   // Lorsqu'une personne écrit !raffle
-  if (message.startsWith('!raffle')) await startRaffle(client, tags, message.toLowerCase());
-  if (message.startsWith('!cancel')) cancelRaffle(client, tags);
-  if (message.startsWith('!join')) joinRaffle(tags);
-
+  if (message.startsWith('!raffle')) startRaffle(client, tags, message.toLowerCase());
+  if (message.startsWith('!cancel') && !self) cancelRaffle(client, tags);
+  if (message.startsWith('!join') && !self) joinRaffle(tags);
+  if (message.startsWith('!cmd') && !self) allCommandes(client);
+  if (containsBeg.test(message) && containsRaffle.test(message) && !self) {
+    begForRaffle(client, tags, message);
+  }
   if (message.startsWith('!timeout'))
     timeout(client, channel, tags, message.toLowerCase().replace(letterNumber, ''));
-
-  if (!checkCooldown(tags['user-id'])) {
+  if (!checkCooldown(tags['user-id']) && !self) {
     checkForPourquoi(client, channel, trunkMessage, tags);
     checkForQuoi(client, channel, trunkMessage, tags);
     checkForQui(client, channel, trunkMessage, tags);
